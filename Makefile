a208ee28b89ad81a140c54dcdd17d0fc124e2092
Mattis Bergmann <mattis.bergmann@gmx.de>
2026-05-29 17:18:29 +0200

    Fixed heap hack

--- Makefile
+++ Makefile
@@ -2,18 +2,24 @@ SOURCEDIR = src
 BUILDDIR = obj
 OUTDIR = dist
 DEPDIR = .deps
+APPNAME = UITemplate
+SYSTEMUI = libs/systemui
 
-AS:=sh4a_nofpueb-elf-gcc
-AS_FLAGS:=-gdwarf-5
-
+OS?=7002
 SDK_DIR?=/sdk
+
+SYSTEMUI_LIB = $(SYSTEMUI)/dist/$(OS)/lib
+SYSTEMUI_AR = $(SYSTEMUI_LIB)/libsystemui.a
 
 DEPFLAGS=-MT $@ -MMD -MP -MF $(DEPDIR)/$*.d
 WARNINGS=-Wall -Wextra -pedantic -Werror -pedantic-errors
-INCLUDES=-I$(SDK_DIR)/include #-I$(SOURCEDIR)
+INCLUDES=-I$(SDK_DIR)/include -I$(SYSTEMUI)/include
 DEFINES=
 FUNCTION_FLAGS=-flto=auto -ffat-lto-objects -fno-builtin -ffunction-sections -fdata-sections -gdwarf-5 -O2
 COMMON_FLAGS=$(FUNCTION_FLAGS) $(INCLUDES) $(WARNINGS) $(DEFINES)
+
+AS:=sh4a_nofpueb-elf-gcc
+AS_FLAGS:=-gdwarf-5
 
 CC:=sh4a_nofpueb-elf-gcc
 CC_FLAGS=-std=c23 $(COMMON_FLAGS)
@@ -22,14 +28,14 @@ CXX:=sh4a_nofpueb-elf-g++
 CXX_FLAGS=-std=c++20 $(COMMON_FLAGS)
 
 LD:=sh4a_nofpueb-elf-g++
-LD_FLAGS:=$(FUNCTION_FLAGS) -Wl,--gc-sections
-LIBS:=-L$(SDK_DIR) -lsdk
+LD_FLAGS:=$(FUNCTION_FLAGS) -Wl,--gc-sections,-Ttext-segment,0x8CC80000
+LIBS:=-L$(SDK_DIR) -L$(SYSTEMUI_LIB) -lsystemui -lsdk
 
 READELF:=sh4a_nofpueb-elf-readelf
 OBJCOPY:=sh4a_nofpueb-elf-objcopy
 STRIP:=sh4a_nofpueb-elf-strip
 
-APP_ELF := $(OUTDIR)/CPapp.elf
+APP_ELF := $(OUTDIR)/$(OS)/$(APPNAME).elf
 APP_HH3 := $(APP_ELF:.elf=.hh3)
 
 AS_SOURCES:=$(shell find $(SOURCEDIR) -name '*.S')
@@ -52,16 +58,20 @@ all: elf hh3
 
 .NOTPARALLEL: clean
 clean:
+	$(MAKE) -C $(SYSTEMUI) $(MAKEFLAGS) clean
 	rm -rf $(BUILDDIR) $(OUTDIR) $(DEPDIR)
 
 %.hh3: %.elf
 	$(STRIP) -o $@ $^
 
-$(APP_ELF): $(OBJECTS)
+$(APP_ELF): $(OBJECTS) | $(SYSTEMUI_AR)
 	@mkdir -p $(dir $@)
 	$(LD) -Wl,-Map $@.map -o $@ $(LD_FLAGS) $^ $(LIBS)
 
 $(NOLTOOBJS): FUNCTION_FLAGS+=-fno-lto
+
+$(SYSTEMUI_AR): phony
+	$(MAKE) -C $(SYSTEMUI) OS=$(OS) SDK_DIR=$(SDK_DIR) all
 
 $(BUILDDIR)/%.o: %.S
 	@mkdir -p $(dir $@)
@@ -81,6 +91,6 @@ compile_commands.json:
 	$(MAKE) $(MAKEFLAGS) clean
 	bear -- sh -c "$(MAKE) $(MAKEFLAGS) --keep-going all || exit 0"
 
-.PHONY: elf hh3 all clean compile_commands.json
+.PHONY: phony elf hh3 all clean compile_commands.json
 
 -include $(DEPFILES)
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,178 @@
 #include <appdef.h>
-#include <cstddef>
-#include <cstdio>
+
+#include <cstdint>
 #include <sdk/os/debug.h>
+#include <sdk/os/lcd.h>
 
-APP_NAME("Your Cool App Name")
-APP_AUTHOR("me")
-APP_DESCRIPTION("A very cool app!")
-APP_VERSION("1.0.0")
+#include "systemui/heap_hack.h"
+#include "systemui/peg.hpp"
 
-int main(int argc, char **argv, char **envp) {
-  std::printf("Arguments:\n");
-  for (std::size_t i = 0; i < static_cast<std::size_t>(argc); i++)
-    std::printf(" - %zu: %s\n", i, argv[i]);
+APP_NAME("PegEditBox Demo")
+APP_AUTHOR("snudget, pho3")
+APP_DESCRIPTION("PegVertList and PegEditBox")
+APP_VERSION("1.1.0")
 
-  std::printf("Environment:\n");
-  for (auto ptr = envp; *ptr != 0; ptr++)
-    std::printf("%s\n", *ptr);
+const WORD TEXTBOX_STYLE = FF_NONE | EF_WRAP | TJ_LEFT;
 
-  std::fflush(stdout);
-  Debug_WaitKey();
+class MyWindow : public PegDecoratedWindow {
+  // Auto-generate element ids, so we don't have to manually set them
+  enum Ids {
+    Id_InfoText = 1,
+    Id_List,
+    Id_Opt_1,
+    Id_Opt_2,
+    Id_Opt_3,
+    Id_Opt_4,
+    Id_Btn,
+    Id_EditBox // New ID for edit box
+  };
+
+public:
+  MyWindow(PegRect &rect) : PegDecoratedWindow(rect) {
+    // This is the title bar of our window. Remember to add it, or you won't be
+    // able to close it
+    auto title = new PegTitle("Testle3", TF_CLOSEBUTTON);
+
+    // These are absolute coordinates.
+    // Since our window starts at x=20, this will give 20 pixels of padding
+    // This method will also recalculate the positions in horizontal mode
+    PegRect textRect;
+    textRect.SetAndCenterIfLandscape(20, 30, 300, 60, TRUE);
+    infoBox = new PegTextBox(textRect, Id_InfoText, TEXTBOX_STYLE,
+                             "Select an option below...");
+
+    Add(title->obj());
+    Add(infoBox->obj());
+
+    // Define the List Area
+    // The list will occupy this specific rectangle.
+    // If items exceed this area, it should scroll (if scrollbars enabled/auto).
+    PegRect listRect;
+    listRect.SetAndCenterIfLandscape(20, 70, 150, 180, TRUE); // Left side
+
+    // Create the Vertical List
+    // FF_THIN: Draws a thin border around the list
+    vertList = new PegVertList(listRect, Id_List, FF_THIN);
+
+    options[0] = new PegRadioButton(0, 0, "Option 1", Id_Opt_1);
+    options[1] = new PegRadioButton(0, 0, "Option 2", Id_Opt_2);
+    options[2] = new PegRadioButton(0, 0, "Option 3", Id_Opt_3);
+    options[3] = new PegRadioButton(0, 0, "Option 4", Id_Opt_4);
+
+    // Add Items to the List
+    // We pass the underlying thin object via ->obj()
+    for (auto item : options) {
+      vertList->Add(item->obj());
+    }
+
+    // Select first option
+    options[0]->SetSelected();
+    vertList->SetSelected(options[0]->obj()); // Using wrapper helper
+    Add(vertList->obj());
+
+    // 3. PegEditBox (Editable Text Area)
+    // Placed to the right of the list or below depending on space.
+    // Here placing it to the right side (Landscape logic assumed mostly)
+    PegRect editRect;
+    editRect.SetAndCenterIfLandscape(160, 70, 300, 180, TRUE);
+
+    mpEditBox =
+        new PegEditBox(editRect, Id_EditBox, FF_RECESSED | EF_EDIT | EF_WRAP);
+
+    // Set initial text
+    mpEditBox->DataSet("Type here...\nLine 2");
+
+    // Enable vertical scrollbar automatically when text exceeds height
+    mpEditBox->SetScrollMode(WSM_AUTOVSCROLL | WSM_CONTINUOUS);
+
+    Add(mpEditBox->obj());
+
+    // 4. Select Button
+    PegRect buttonRect;
+    buttonRect.SetAndCenterIfLandscape(110, 200, 210, 230, TRUE);
+    auto button =
+        new PegTextButton(buttonRect, "Check", Id_Btn, AF_ENABLED | TJ_CENTER);
+    Add(button->obj());
+  }
+
+  SIGNED Message(const PegMessage &mesg) override {
+    const char *strings[4] = {
+        "Opt 1",
+        "Opt 2",
+        "Opt 3",
+        "Opt 4",
+    };
+
+    // All events can be handled in a single switch
+    switch (mesg.wType) {
+    // Detect select button click
+    case PEG_SIGNAL(Id_Btn, PSF_CLICKED):
+      // On button click, we update the status box with current selection
+      // AND append some text to the EditBox to demonstrate API usage.
+
+      infoBox->DataSet("Processing...");
+
+      for (int i = 0; i < 4; i++) {
+        if (options[i]->IsSelected()) {
+          infoBox->DataSet(strings[i]);
+
+          // Append selection to edit box
+          // Note: Append automatically handles redrawing if bDraw is true
+          // (default)
+          mpEditBox->Append("\nSelected: ");
+          mpEditBox->Append(strings[i]);
+        }
+      }
+
+      Screen()->Invalidate(infoBox->obj()->mClip);
+
+      // Redraw the text
+      infoBox->Draw();
+      return 0;
+    }
+
+    // We still want events like the close button to be handled
+    return PegDecoratedWindow::Message(mesg);
+  }
+
+private:
+  PegTextBox *infoBox;
+  PegVertList *vertList;
+  PegRadioButton *options[4];
+  PegEditBox *mpEditBox; // Pointer to our new EditBox
+};
+
+const size_t CODE_REGION_SIZE = 2 * 1024 * 1024;
+
+extern "C" void calcInit() {
+  // Allocate 1MB of memory at the end of the heap for our program
+  auto *ptr = initFixedRegion(CODE_REGION_SIZE);
+
+  // Not enough heap space, reset the system to recreate the heap
+  if (ptr != reinterpret_cast<void *>(0x8cc80000))
+    heapReset();
+}
+
+extern "C" void calcExit() {
+  // Call the OS out-of-memory logic to remove our heap abuse
+  uninitFixedRegion(CODE_REGION_SIZE);
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv,
+         [[maybe_unused]] char **envp) {
+  // TODO: Should be called by the loader before the actual binary is loaded.
+  // But since we reset the heap anyways if there is data, this shouldn't matter
+  calcInit();
+
+  // Make window slightly larger to fit everything nicely
+  PegRect rectWin(10, 10, 310, 280);
+  auto win = new MyWindow(rectWin);
+
+  // Show the window. This will also delete all the elements when it is closed
+  win->Execute();
+  // But it won't delete the wrapper class
+  // WARNING: This may change in the future, not sure yet
+  delete win;
+
+  calcExit();
 }
